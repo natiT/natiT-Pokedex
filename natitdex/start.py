67a76f147b7953861e7e315155daff76951fa19b
Philipp Peter <philipppeter89@gmail.com>
2022-09-26 18:23:52 +0200

    more fixes

--- natitdex/start.py
+++ natitdex/start.py
@@ -19,36 +19,41 @@ from natitdex.classes import api_output
 ONE_DAY = 60 * 60 * 24
 PORT = 80
 
+
 def main():
     world_name_list = "https://raw.githubusercontent.com/PokeAPI/pokeapi/master/data/v2/csv/pokemon_species_names.csv"
     csv_name_list = pandas.read_csv(world_name_list)
 
     lang_id = 6
-    pkmn_list = csv_name_list[["pokemon_species_id", "local_language_id", "name"]]
+    pkmn_list = csv_name_list[[
+        "pokemon_species_id", "local_language_id", "name"]]
 
     from fastapi import FastAPI
+    from fastapi.responses import PlainTextResponse
     app = FastAPI()
 
-    @app.get("/dex/{pokemon}")
+    @app.get("/dex/{pokemon}", response_class=PlainTextResponse)
     async def read_pokemon(pokemon):
-        pokemon = pokemon.replace("!dex ",'')
+        pokemon = pokemon.replace("!dex ", '')
         print(pokemon)
         language_name = get_lang_name_from_id(lang_id)
-        if(language_name == 'none'):
-            return f"{pokemon} not exist in Database"
+
         if (language_name == "error"):
             return "The Language ID in the Script was not correct. Error can only be resolved by the hoster"
         if (pokemon.isnumeric() == True):
             pkmn_lang_name = pokemon
             pkmn_id = pokemon
         else:
-            pkmn_lang_name = get_pkmnid_and_pkmnname_from_string(pkmn_list, pokemon, lang_id)
-            pkmn_id = pkmn_lang_name['pokemon_species_id'].to_string(index=False)
-        # pkmn_name = pkmn_lang_name['name'].to_string(index=False)
-        # print(pkmn_id)
-        # return(pkmn_id + " " + pkmn_name)
-        api_pkmn = get_pkmn_from_pokeapi(pkmn_id, lang_id, pkmn_lang_name, pkmn_list, language_name)
-        out = f"Pokedex Eintrag für {api_pkmn.name} - Typ: {api_pkmn.types} - Fähigkeiten: {api_pkmn.abilities} - Basiswerte: {api_pkmn.stats}"
+            pkmn_lang_name = get_pkmnid_and_pkmnname_from_string(
+                pkmn_list, pokemon, lang_id)
+            if isinstance(pkmn_lang_name, pandas.DataFrame):
+                pkmn_id = pkmn_lang_name['pokemon_species_id'].to_string(
+                    index=False)
+                api_pkmn = get_pkmn_from_pokeapi(
+                    pkmn_id, lang_id, pkmn_lang_name, pkmn_list, language_name)
+                out = f"Pokedex Eintrag für {api_pkmn.name} - Typ: {api_pkmn.types} - Fähigkeiten: {api_pkmn.abilities} - Basiswerte: {api_pkmn.stats}"
+            else:
+                return f"{pokemon} not exist in Database"
 
         # return api_pkmn
         # out = get_pkmn_from_pokeapi(pkmn_id)
