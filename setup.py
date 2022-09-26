7b9de25cab51658dca13ec0de0d4d313212938db
Philipp Peter <philipppeter89@gmail.com>
2022-09-26 15:59:13 +0200

    requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from importlib.metadata import entry_points
 from setuptools import setup
 from setuptools import find_packages
 
@@ -9,11 +8,12 @@ setup(
     author='natiT',
     author_email='info@natit.de',
     url='github.com',
+
     packages=find_packages(exclude=('test*')),
     entry_points={
         'console_scripts': [
             'natidex = natitdex.main:main'
         ]
-    }
-
+    },
+    install_requires=['pandas', 'urllib3', 'fastapi',"cachetools", "uvicorn"]
 )
